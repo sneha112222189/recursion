0943f428c7d6f834d2ced80b5409af7d9ae10dc5
sneha112222189 <snehagangwarbhr@gmail.com>
2025-11-12 09:58:38 +0530

    save changes

---
+++ boredom.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,1,2,2,2,2,2,3,3,3,4};
+        int[] dp = new int[arr.length];
+        for(int x = 0; x < dp.length; x++) {dp[x]=-1;}
+        Arrays.sort(arr);
+        int max=arr[arr.length-1];
+        int[] freq=new int[max+1];
+        for(int i=0;i<freq.length;i++){
+            freq[i]=0;
+            for(int j=0;j<arr.length;j++){
+                if(arr[j]==i){
+                    freq[i]++;
+                }
+            }
+        }
+        int result = boredom(freq,0,freq.length-1,dp);
+        System.out.println(result);
+    }
+
+    static int boredom(int[] freq,int i,int j,int[] dp){
+        if(i>j) return 0;
+
+        if(dp[i] != -1) return dp[i];
+        int sum1 = i*freq[i] + boredom(freq,i+2,j,dp);
+        int sum2 = boredom(freq,i+1,j,dp);
+        dp[i] = Math.max(sum1,sum2);
+        return dp[i];
+        }
+    } 
+    
+
